4b98ca0336a9ddfb21f8162d6ea1e9746675a4d6
Allen <Allenlea2007@gmail.com>
2021-04-19 15:22:24 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import { WorldMap } from "react-svg-worldmap"
 
 function App() {
+  const data =
+    [
+      { "country": "cn", value: 5 }, // china
+      { "country": "us", value: 10 },  // united states
+      { "country": "ru", value: 7 },  // russia
+    ]
+    
+  const stylingFunction = (context) => {
+    console.log(context)
+    const opacityLevel = 0.1 + (1.5 * (context.countryValue - context.minValue) / (context.maxValue - context.minValue))
+    return { fill: context.country === "US" ? "blue" : context.color, fillOpacity: opacityLevel, stroke: "green", strokeWidth: 1, strokeOpacity: 0.2, cursor: "pointer" }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < div className="App" >
+      < div className="Main">
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <WorldMap 
+                color={"red"} 
+                tooltipBgColor={"#D3D3D3"} 
+                title="Custom Style Test" 
+                valueSuffix="points" 
+                size="xl" 
+                data={data} 
+                frame={true} 
+                styleFunction={stylingFunction}/>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
